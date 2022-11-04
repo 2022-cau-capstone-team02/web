d1cf9ce5dfb3f509c8275f37d783a2754d8db014
sjly3k <sjly3k@gmail.com>
2022-11-04 16:38:09 +0900

    fix prettier

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import AssetInfo from './components/AssetInfo';
 import Assets from './components/Assets';
 import _Navbar from './components/Navbar';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {QueryClientProvider, QueryClient, useQuery } from 'react-query';
+import { QueryClientProvider, QueryClient, useQuery } from 'react-query';
 // import Sidebar from './components/Sidebar';
 
 const apiData = [];
@@ -15,10 +15,10 @@ let cnt = 0;
 
 const StyledContainer = styled(Container)`
   margin-top: 90px;
-  background-color: #F5F7FB;
-  padding : 0 auto;
+  background-color: #f5f7fb;
+  padding: 0 auto;
   height: 100%;
-`
+`;
 
 const AssetWrapper = styled.div`
   width: 100%;
@@ -27,66 +27,66 @@ const AssetWrapper = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`
+`;
 
-async function fetchUserData(){
-  return await axios.get('/userInfo', {responseType:'json'});
+async function fetchUserData() {
+  return await axios.get('/userInfo', { responseType: 'json' });
 }
 
 function Dashboard() {
   const [show, setShow] = useState(false);
   const { data, isLoading } = useQuery('userInfo', fetchUserData);
   const [params, setParams] = useState({
-    key:process.env.REACT_APP_YOUTUBE_API_KEY,
-    id:'',
+    key: process.env.REACT_APP_YOUTUBE_API_KEY,
+    id: '',
     part: 'id,snippet,contentDetails,statistics',
   });
 
-
-  
-  async function fetchYoutubeData(){
-    await axios.get('https://www.googleapis.com/youtube/v3/channels', {params})
-    .then((res)=>{
-      apiData.push(res.data.items);
-      cnt = cnt+1;
-      setShow(true);
-    })
-    .catch(err=>console.log(err));
+  async function fetchYoutubeData() {
+    await axios
+      .get('https://www.googleapis.com/youtube/v3/channels', { params })
+      .then((res) => {
+        apiData.push(res.data.items);
+        cnt = cnt + 1;
+        setShow(true);
+      })
+      .catch((err) => console.log(err));
   }
-  useEffect(()=>{
-    if(isLoading === false){
+  useEffect(() => {
+    if (isLoading === false) {
       let channel = '';
-      data.data.data[0].stake.forEach(element => {
+      data.data.data[0].stake.forEach((element) => {
         channel += element;
       });
-      setParams(()=>{
-        let copy = {...params};
-        return {...copy, id: channel};
+      setParams(() => {
+        let copy = { ...params };
+        return { ...copy, id: channel };
       });
     }
-  },[isLoading])
+  }, [isLoading]);
 
-  useEffect(()=>{
-    if(params.id != ''){
+  useEffect(() => {
+    if (params.id != '') {
       fetchYoutubeData();
     }
-  },[params])
-  
-  if(isLoading){
-    return 
-      <div>Loading..</div>
+  }, [params]);
+
+  if (isLoading) {
+    return <div>Loading..</div>;
   }
+
   return (
     <React.Fragment>
-      { show ?  
-      <StyledContainer fluid>
-        <AssetWrapper>
-          <AssetInfo apiData={apiData}/>
-          <Assets apiData={apiData}/>
-        </AssetWrapper>
-      </StyledContainer> : null} 
-    </React.Fragment> 
-  ) 
+      {show ? (
+        <StyledContainer fluid>
+          <AssetWrapper>
+            <AssetInfo apiData={apiData} />
+            <Assets apiData={apiData} />
+          </AssetWrapper>
+        </StyledContainer>
+      ) : null}
+    </React.Fragment>
+  );
 }
 
 export default Dashboard;
